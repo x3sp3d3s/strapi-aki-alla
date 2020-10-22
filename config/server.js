77c2ef269a5c6ff2bd32f2d72d26d186c495ef72
x3sp3d3s <albertcespedes@gmail.com>
2020-10-22 15:59:23 +0200

    Base de datos creado con strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b937225076c403a61b84a5dd753a4c7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e384c8dd-2e62-472c-a84d-ceb9bc008ad9'
+};
